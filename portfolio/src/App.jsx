e3eb00137d3403ba8fcf2b0c692376d0da68c55d
drejmin <djjamn@gmail.com>
2023-09-20 11:59:16 -0700

    modified:   portfolio/src/App.jsx 	deleted:    portfolio/src/Components/Images/Email-Logo-No-Background.jpg 	deleted:    portfolio/src/Components/Images/GitHub-Logo.png 	new file:   portfolio/src/Components/Images/email-1573-svgrepo-com.svg 	deleted:    portfolio/src/Components/Images/linkedin-png-linkedin-icon-1600.png 	new file:   portfolio/src/Components/Images/linkedin-svgrepo-com.svg 	new file:   portfolio/src/Components/Images/logo-github-svgrepo-com.svg

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -25,9 +25,9 @@ function App() {
         <section id="contact">
           <h2>Contact</h2>
           <div>
-            <a href="mailto:deandremi@yahoo.com" ><img src="portfolio/src/Components/Images/Email-Logo-No-Background.jpg" alt='email icon'/></a>
-            <a href="https://github.com/drejmin"><img src="portfolio/src/Components/Images/GitHub-Logo.png" alt='github icon'/></a>
-            <a href="www.linkedin.com/in/deandre-minor92"><img src="portfolio/src/Components/Images/linkedin-png-linkedin-icon-1600.png" alt='linkedin icon'/></a>
+            <a href="mailto:deandremi@yahoo.com" ><img src="portfolio/src/Components/Images/email-1573-svgrepo-com.svg" alt='email icon'/></a>
+            <a href="https://github.com/drejmin"><img src="portfolio/src/Components/Images/logo-github-svgrepo-com.svg" alt='github icon'/></a>
+            <a href="www.linkedin.com/in/deandre-minor92"><img src="portfolio/src/Components/Images/linkedin-svgrepo-com.svg" alt='linkedin icon'/></a>
           </div>
         </section>
       </body>
