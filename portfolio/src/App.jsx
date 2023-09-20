182059680c644aa65a2dcbf7858aee57ca3c884a
drejmin <djjamn@gmail.com>
2023-09-20 11:54:11 -0700

    modified:   portfolio/src/App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -25,9 +25,9 @@ function App() {
         <section id="contact">
           <h2>Contact</h2>
           <div>
-            <a href="mailto:deandremi@yahoo.com" ><img src="portfolio/src/Components/Images/Email-Logo-No-Background.jpg" alt='icon'/></a>
-            <a href="https://github.com/drejmin"><img src="portfolio/src/Components/Images/GitHub-Logo.png" alt='icon'/></a>
-            <a href="www.linkedin.com/in/deandre-minor92"><img src="portfolio/src/Components/Images/linkedin-png-linkedin-icon-1600.png" alt='icon'/></a>
+            <a href="mailto:deandremi@yahoo.com" ><img src="portfolio/src/Components/Images/Email-Logo-No-Background.jpg" alt='email icon'/></a>
+            <a href="https://github.com/drejmin"><img src="portfolio/src/Components/Images/GitHub-Logo.png" alt='github icon'/></a>
+            <a href="www.linkedin.com/in/deandre-minor92"><img src="portfolio/src/Components/Images/linkedin-png-linkedin-icon-1600.png" alt='linkedin icon'/></a>
           </div>
         </section>
       </body>
