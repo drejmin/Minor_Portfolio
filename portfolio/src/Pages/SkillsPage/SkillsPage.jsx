c7f0271a29667c194665a0e6d30f98d44a73bad9
drejmin <djjamn@gmail.com>
2023-09-20 15:12:19 -0700

    modified:   portfolio/src/Pages/SkillsPage/SkillsPage.jsx

--- portfolio/src/Pages/SkillsPage/SkillsPage.jsx
+++ portfolio/src/Pages/SkillsPage/SkillsPage.jsx
@@ -4,25 +4,25 @@ export default function SkillsPage(){
             <div class="about__content-skills">
                     <h3 class="about__content-title">My Skills</h3>
                     <div class="skills">
-                        <div class="skills__skill">JavaScript</div>
-                        <div class="skills__skill">HTML</div>
-                        <div class="skills__skill">Python</div>
-                        <div class="skills__skill">CSS</div>
-                        <div class="skills__skill">VSCode</div>
-                        <div class="skills__skill">React</div>
-                        <div class="skills__skill">GIT</div>
-                        <div class="skills__skill">Express</div>
-                        <div class="skills__skill">SQL</div>
-                        <div class="skills__skill">MongoDB</div>
-                        <div class="skills__skill">EJS</div>
-                        <div class="skills__skill">DTL</div>
-                        <div class="skills__skill">Django</div>
-                        <div class="skills__skill">Microsoft Suite</div>
-                        <div class="skills__skill">Google Suite</div>
-                        <div class="skills__skill">RESTful Routing</div>
-                        <div class="skills__skill">JSX</div>
-                        <div class="skills__skill">NodeJS</div>
-                        <div class="skills__skill">GIT Version Control</div>
+                        <div class="skills__skill">JavaScript <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">HTML <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">Python <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">CSS <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">VSCode <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">React <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">GIT <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">Express <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">SQL <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">MongoDB <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">EJS <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">DTL <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">Django <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">Microsoft Suite <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">Google Suite <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">RESTful Routing <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">JSX <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">NodeJS <progress id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">GIT Version Control <progress id="progressBar" value="25" max="100"></progress></div>
                     </div>
             </div>
 
