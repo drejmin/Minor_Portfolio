38ccff79ed52646b861daf0aa56739add2bbc814
drejmin <djjamn@gmail.com>
2023-09-21 15:04:02 -0700

    modified:   portfolio/src/Pages/SkillsPage/SkillsPage.css 	modified:   portfolio/src/Pages/SkillsPage/SkillsPage.jsx

--- portfolio/src/Pages/SkillsPage/SkillsPage.jsx
+++ portfolio/src/Pages/SkillsPage/SkillsPage.jsx
@@ -6,25 +6,24 @@ export default function SkillsPage(){
             <div class="content-skills">
                     <h3 class="content-title">My Skills</h3>
                     <div class="skills">
-                        <div class="skills__skill">JavaScript <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">HTML <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">Python <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">CSS <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">VSCode <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">React <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">GIT <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">Express <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">SQL <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">MongoDB <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">EJS <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">DTL <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">Django <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">Microsoft Suite <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">Google Suite <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">RESTful Routing <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">JSX <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">NodeJS <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
-                        <div class="skills__skill">GIT Version Control <progress class = 'progress'id="progressBar" value="25" max="100"></progress></div>
+                        <div class="skills__skill">JavaScript <progress class = 'progress'id="progressBar" value="60" max="100"></progress></div>
+                        <div class="skills__skill">HTML <progress class = 'progress'id="progressBar" value="58" max="100"></progress></div>
+                        <div class="skills__skill">Python <progress class = 'progress'id="progressBar" value="55" max="100"></progress></div>
+                        <div class="skills__skill">CSS <progress class = 'progress'id="progressBar" value="47" max="100"></progress></div>
+                        <div class="skills__skill">VSCode <progress class = 'progress'id="progressBar" value="80" max="100"></progress></div>
+                        <div class="skills__skill">React <progress class = 'progress'id="progressBar" value="65" max="100"></progress></div>
+                        <div class="skills__skill">GIT <progress class = 'progress'id="progressBar" value="80" max="100"></progress></div>
+                        <div class="skills__skill">Express <progress class = 'progress'id="progressBar" value="65" max="100"></progress></div>
+                        <div class="skills__skill">SQL <progress class = 'progress'id="progressBar" value="45" max="100"></progress></div>
+                        <div class="skills__skill">MongoDB <progress class = 'progress'id="progressBar" value="65" max="100"></progress></div>
+                        <div class="skills__skill">EJS <progress class = 'progress'id="progressBar" value="47" max="100"></progress></div>
+                        <div class="skills__skill">Django <progress class = 'progress'id="progressBar" value="65" max="100"></progress></div>
+                        <div class="skills__skill">Microsoft Suite <progress class = 'progress'id="progressBar" value="83" max="100"></progress></div>
+                        <div class="skills__skill">Google Suite <progress class = 'progress'id="progressBar" value="92" max="100"></progress></div>
+                        <div class="skills__skill">RESTful Routing <progress class = 'progress'id="progressBar" value="56" max="100"></progress></div>
+                        <div class="skills__skill">JSX <progress class = 'progress'id="progressBar" value="60" max="100"></progress></div>
+                        <div class="skills__skill">NodeJS <progress class = 'progress'id="progressBar" value="49" max="100"></progress></div>
+                        <div class="skills__skill">GIT Version Control <progress class = 'progress'id="progressBar" value="70" max="100"></progress></div>
                     </div>
             </div>
 
