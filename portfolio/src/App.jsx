f598f9f9af1f3f3dd18c4b648214a88422039f0c
drejmin <djjamn@gmail.com>
2023-09-21 15:14:32 -0700

    modified:   portfolio/src/App.jsx 	new file:   portfolio/src/Components/Images/GitHub-Logo.png 	new file:   portfolio/src/Components/Images/download icon.webp 	deleted:    portfolio/src/Components/Images/download-interface-symbol-svgrepo-com.svg 	new file:   portfolio/src/Components/Images/email icon.png 	deleted:    portfolio/src/Components/Images/email-1573-svgrepo-com.svg 	new file:   portfolio/src/Components/Images/linkedin logo.png 	deleted:    portfolio/src/Components/Images/linkedin-svgrepo-com.svg 	deleted:    portfolio/src/Components/Images/logo-github-svgrepo-com.svg 	modified:   portfolio/src/Pages/HomePage/HomePage.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -18,6 +18,15 @@ function App() {
           <Route path="/ProjectsPage" element={<ProjectsPage/>} />
       </Routes>
       </>
+      <section id="contact">
+                <h2>Contact</h2>
+                <div>
+                    <a href="https://docs.google.com/document/d/1IG3LCk6lxiGOLJM3HiRph8jTf7U4lWoczZCQMlfMTaE/edit?usp=sharing" ><img src="portfolio/src/Components/Images/download icon.webp" alt='download icon'/></a>
+                    <a href="mailto:deandremi@yahoo.com" ><img src="portfolio/src/Components/Images/email icon.png" alt='email icon'/></a>
+                    <a href="https://github.com/drejmin"><img src="portfolio/src/Components/Images/GitHub-Logo.png" alt='github icon'/></a>
+                    <a href="www.linkedin.com/in/deandre-minor92"><img src="portfolio/src/Components/Images/linkedin logo.png" alt='linkedin icon'/></a>
+                </div>
+                </section>
     </main>
   );
 }
--- portfolio/src/Pages/HomePage/HomePage.jsx
+++ portfolio/src/Pages/HomePage/HomePage.jsx
@@ -6,15 +6,6 @@ export default function HomePage(){
                 <header className="App-header">
                     <h1>Hey, I'm DeAndre! Welcome to my Portfolio!</h1>
                 </header>
-                <section id="contact">
-                <h2>Contact</h2>
-                <div>
-                    <a href="https://docs.google.com/document/d/1IG3LCk6lxiGOLJM3HiRph8jTf7U4lWoczZCQMlfMTaE/edit?usp=sharing" ><img src="portfolio/src/Components/Images/download-interface-symbol-svgrepo-com.svg" alt='download icon'/></a>
-                    <a href="mailto:deandremi@yahoo.com" ><img src="portfolio/src/Components/Images/email-1573-svgrepo-com.svg" alt='email icon'/></a>
-                    <a href="https://github.com/drejmin"><img src="portfolio/src/Components/Images/logo-github-svgrepo-com.svg" alt='github icon'/></a>
-                    <a href="www.linkedin.com/in/deandre-minor92"><img src="portfolio/src/Components/Images/linkedin-svgrepo-com.svg" alt='linkedin icon'/></a>
-                </div>
-                </section>
             </body>
       
         </main>
