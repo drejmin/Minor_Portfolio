9f16f21a55a3e76037138ef8ff5548c69de7aec7
drejmin <djjamn@gmail.com>
2023-09-21 15:34:35 -0700

    modified:   portfolio/src/App.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -21,10 +21,10 @@ function App() {
       <section id="contact">
                 <h2>Contact</h2>
                 <div>
-                    <a href="https://docs.google.com/document/d/1IG3LCk6lxiGOLJM3HiRph8jTf7U4lWoczZCQMlfMTaE/edit?usp=sharing" ><img class = 'logo' src="portfolio/src/Components/Images/download icon.webp" alt='download icon'/></a>
-                    <a href="mailto:deandremi@yahoo.com" ><img class = 'logo' src="portfolio/src/Components/Images/email icon.png" alt='email icon'/></a>
-                    <a href="https://github.com/drejmin"><img class = 'logo' src="portfolio/src/Components/Images/GitHub-Logo.png" alt='github icon'/></a>
-                    <a href="www.linkedin.com/in/deandre-minor92"><img class = 'logo' src="portfolio/src/Components/Images/linkedin logo.png" alt='linkedin icon'/></a>
+                    <a href="https://docs.google.com/document/d/1IG3LCk6lxiGOLJM3HiRph8jTf7U4lWoczZCQMlfMTaE/edit?usp=sharing" ><img class = 'logo' src={"portfolio/src/Components/Images/download icon.png"} alt='download icon'/></a>
+                    <a href="mailto:deandremi@yahoo.com" ><img class = 'logo' src={"portfolio/src/Components/Images/email icon.png"} alt='email icon'/></a>
+                    <a href="https://github.com/drejmin"><img class = 'logo' src={"portfolio/src/Components/Images/GitHub-Logo.png"} alt='github icon'/></a>
+                    <a href="www.linkedin.com/in/deandre-minor92"><img class = 'logo' src={"portfolio/src/Components/Images/linkedin logo.png"} alt='linkedin icon'/></a>
                 </div>
                 </section>
     </main>
