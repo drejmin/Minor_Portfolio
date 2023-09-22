2712bee1759cdf28be32f23964c08d1eb12f01b0
drejmin <djjamn@gmail.com>
2023-09-21 20:23:54 -0700

    modified:   portfolio/src/App.jsx 	modified:   portfolio/src/Components/NavBar/NavBar.jsx

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
       <>
       <Navbar/>
       <Routes>
-          <Route path="/HomePage" exact element={<HomePage/>} />
+          <Route path="/" exact element={<HomePage/>} />
           <Route path="/AboutPage" element={<AboutPage/>} />
           <Route path="/SkillsPage" element={<SkillsPage/>} />
           <Route path="/ProjectsPage" element={<ProjectsPage/>} />
--- portfolio/src/Components/NavBar/NavBar.jsx
+++ portfolio/src/Components/NavBar/NavBar.jsx
@@ -6,7 +6,7 @@ export default function NavBar(){
   return (
     <nav class= "navbar">
       <ul>
-        <li class = 'choice'><Link class = 'choice' to="/HomePage">Home</Link></li>
+        <li class = 'choice'><Link class = 'choice' to="/">Home</Link></li>
         <li class = 'choice'><Link class = 'choice' to="/ProjectsPage">Projects</Link></li>
         <li class = 'choice'><Link class = 'choice' to="/SkillsPage">Skills</Link></li>
         <li class = 'choice'><Link class = 'choice' to="/AboutPage">About</Link></li>
